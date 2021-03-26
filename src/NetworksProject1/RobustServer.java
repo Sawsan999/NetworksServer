3d8d95dd8c996b14d82a9ae1e22760b79eba7c82
Sawsan El Zahr <66962214+Sawsan999@users.noreply.github.com>
2021-03-26 18:40:51 +0200

    updates

--- src/NetworksProject1/RobustServer.java
+++ src/NetworksProject1/RobustServer.java
@@ -1,11 +1,8 @@
 package NetworksProject1;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.io.*;
+import java.net.*;
+import java.util.*;
 import java.util.concurrent.*;
 
 
@@ -19,6 +16,10 @@ public class RobustServer {
         try (ServerSocket welcomeSocket = new ServerSocket(6789);) {
             System.out.println("Server up and running, waiting for requests....");
             ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(100);
+            TreeMap<String, Integer> tmap = new TreeMap<String, Integer>();
+            int maxCon=10;
+            PeriodicTimer(tmap);
+
             while (true) {
                 // for visualization purpose, the clientId will count the clients and display which one is processed.
                 // wait on welcoming socket for contact by client, creates a new socket
@@ -27,21 +28,56 @@ public class RobustServer {
                 clientId++;//a new client is connected
                 System.out.println("Client " + clientId + " connected at: " + connectionSocket.getInetAddress().getHostAddress());
 
-                // create and submit new runnable to executor, if thread pool limit is already reached then the current client will wait in a queue
-                ClientHandler clientSock = new ClientHandler(connectionSocket, clientId);
-                executor.submit(clientSock);
+                if(BlockedIP(maxCon, tmap,connectionSocket.getInetAddress().getHostAddress())){
+                    System.out.println("Connection blocked from "+connectionSocket.getInetAddress().getHostAddress());
+                }
+                else {
+                    // create and submit new runnable to executor, if thread pool limit is already reached then the current client will wait in a queue
+                    ClientHandler clientSock = new ClientHandler(connectionSocket, clientId);
+                    executor.submit(clientSock);
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    // ClientHandler class 
+    private static boolean BlockedIP(int maxCon, TreeMap<String, Integer> tmap, String hostAddress) {
+        boolean flag=false;
+        if (!tmap.containsKey(hostAddress)){
+            tmap.put(hostAddress,1);
+        }
+        else{
+            int count=tmap.get(hostAddress)+1;
+            tmap.put(hostAddress, count);
+            if(count>=maxCon){flag=true;}
+        }
+        System.out.println(tmap);
+        return flag;
+    }
+
+    public static void PeriodicTimer(TreeMap<String, Integer> tmap) {
+        TimerTask repeatedTask = new TimerTask() {
+            public void run() {
+                tmap.clear();
+                System.out.println("Array updated");
+                System.out.println(tmap);
+            }
+        };
+        Timer timer = new Timer("Timer");
+
+        long delay = 0;
+        long period = 1000*60;
+        timer.scheduleAtFixedRate(repeatedTask, delay, period);
+    }
+
+
+    // ClientHandler class
     private static class ClientHandler implements Runnable {
         private final Socket clientSocket;
         private final int clientId;
 
-        // Constructor 
+        // Constructor
         public ClientHandler(Socket socket, int clientId) {
             this.clientSocket = socket;
             this.clientId = clientId;
